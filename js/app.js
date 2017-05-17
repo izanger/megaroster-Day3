5608c7eba0f48548f94912ff05412749736d9322
Ian Zanger <izanger@purdue.edu>
2017-05-17 15:53:11 -0400

    Fix move functions so students array is rearranged

--- js/app.js
+++ js/app.js
@@ -50,6 +50,7 @@ const megaroster = {
     const btn = ev.target 
     const id = parseInt(btn.parentElement.parentElement.dataset.id)
     if(id === megaroster.students[0].id){
+      alert()
       return;
     }
     const li = btn.closest('.student')
@@ -64,12 +65,27 @@ const megaroster = {
     const newLi = megaroster.buildListItem(stud)
     li.parentElement.insertBefore(newLi, li.previousElementSibling)
     li.remove()
+
+    //rearrange students array
+    for(let i = 0; i < megaroster.students.length; i++){
+      if(id === megaroster.students[i].id){
+        const tempId = megaroster.students[i].id
+        const tempName = megaroster.students[i].name
+        megaroster.students[i].id = megaroster.students[i-1].id
+        megaroster.students[i].name = megaroster.students[i-1].name
+        megaroster.students[i-1].id = tempId
+        megaroster.students[i-1].name = tempName
+        break
+      }
+    }
+
   },
 
   moveDown(ev) {
     const btn = ev.target
     const id = parseInt(btn.parentElement.parentElement.dataset.id)
     if(id === megaroster.students[megaroster.students.length - 1].id){
+      alert()
       return;
     }
     const li = btn.closest('.student')
@@ -83,6 +99,19 @@ const megaroster = {
     const newLi = megaroster.buildListItem(stud)
     li.parentElement.insertBefore(newLi, li.nextElementSibling.nextElementSibling)
     li.remove()
+
+    //rearrange students array
+    for(let j = 0; j < megaroster.students.length; j++){
+      if(id === megaroster.students[j].id){
+        const tempId = megaroster.students[j].id
+        const tempName = megaroster.students[j].name
+        megaroster.students[j].id = megaroster.students[j+1].id
+        megaroster.students[j].name = megaroster.students[j+1].name
+        megaroster.students[j+1].id = tempId
+        megaroster.students[j+1].name = tempName
+        break
+      }
+    }    
   },
 
   addStudent(ev) {
